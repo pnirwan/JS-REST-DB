4b56aaf616c888211ae6720442467ea5edde3db9
deep <deep@deep-300E4Z-300E5Z-300E7Z.(none)>
2014-04-01 20:52:27 +0530

    delete calls

--- routes/user.js
+++ routes/user.js
@@ -13,19 +13,41 @@ exports.get_call = function(req, res) {
         // if requested database exists we gonna "use" that database
         connection.query('USE ' + req.param("db_name"), function(err, rows) {
             if (typeof (req.param("table_name")) !== 'undefined') {
-                
-                if (typeof (req.query.limit) !== 'undefined'){
+                if (typeof (req.query.limit) !== 'undefined') {
                     // if limit we gonna put limit in query
-                    connection.query('SELECT * FROM ' + req.param("table_name") + ' LIMIT '+ req.query.limit, function(err, rows) {
-                    res.json({data: rows});
-                });
-                    
+                    connection.query('SELECT * FROM ' + req.param("table_name") + ' LIMIT ' + req.query.limit, function(err, rows) {
+                        res.json({data: rows});
+                    });
                 }
-                
+                else{
+                    res.json({message: 'limit is not defined'});
+                }
+                if ((typeof (req.query.by) !== 'undefined') && (typeof (req.query.order) !== 'undefined')) {
+                    if (typeof (req.query.limit) !== 'undefined') {
+                        console.log('SELECT * FROM ' + req.param("table_name") + ' ORDER BY ' + req.query.by + ' ' + req.query.order + ' LIMIT ' + req.query.limit);
+                        // if order by field name and limit comes we gonna put order by amd limit in query
+                        connection.query('SELECT * FROM ' + req.param("table_name") + ' ORDER BY ' + req.query.by + ' ' + req.query.order + ' LIMIT ' + req.query.limit, function(err, rows) {
+                            res.json({data: rows});
+                        });
+                    }
+                    else{
+                        res.json({message: 'either order by or limit is not defined'})
+                    }
+                    // if order by field name comes we gonna put order by in query
+                    connection.query('SELECT * FROM ' + req.param("table_name") + ' ORDER BY ' + req.query.by + ' ' + req.query.order, function(err, rows) {
+                        res.json({data: rows});
+                    });
+                }
+                else{
+                    res.json({message: 'order by is not defined'})
+                }
                 // if table name exists we gonna execute    
                 connection.query('SELECT * FROM ' + req.param("table_name"), function(err, rows) {
                     res.json({data: rows});
                 });
+            }
+            else{
+                res.json({message: 'table is not defined'})
             }
         });
     }
@@ -37,68 +59,107 @@ exports.get_callbyid = function(req, res) {
         connection.query('USE ' + req.param("db_name"), function(err, rows) {
             if ((typeof (req.param("table_name")) !== 'undefined')&& (typeof (req.param("id")) !== 'undefined') ){
                 // if table name exists we gonna execute 
-                
                 connection.query('SELECT * FROM ' + req.param("table_name") + ' WHERE id = ' + req.param("id") , function(err, rows) {
                     res.json({data: rows});
                 });
             }
+            else{
+                res.json({message: 'either table name is not defined or id is not defined'})
+            }
         });
     }
 };
-exports.get_callbyfield_name = function(req, res) {
+exports.get_callbylike = function(req, res) {
     if (typeof (req.param("db_name")) !== 'undefined') {
         // if requested database exists we gonna "use" that database
         connection.query('USE ' + req.param("db_name"), function(err, rows) {
             if ((typeof (req.param("table_name")) !== 'undefined')&& (typeof (req.param("column_name")) !== 'undefined')&& (typeof (req.param("value")) !== 'undefined')){
                 // if table name exists we gonna execute 
-                
                 connection.query('SELECT * FROM ' + req.param("table_name") + ' WHERE ' + req.param("column_name") + ' LIKE "%'+req.param("value")+'%"' , function(err, rows) {
                     res.json({data: rows});
                 });
             }
-        });
-    }
-};
-exports.get_callbylimit = function(req, res) {
-        console.log("deep");
-    if (typeof (req.param("db_name")) !== 'undefined') {
-    
-        // if requested database exists we gonna "use" that database
-        connection.query('USE ' + req.param("db_name"), function(err, rows) {
-            if ((typeof (req.param("table_name")) !== 'undefined')&& (typeof (req.query("limit")) !== 'undefined')){
-                // if table name exists we gonna execute 
-                console.log('SELECT * FROM ' + req.param("table_name") + '?limit=' + req.query("limit"));
-                connection.query('SELECT * FROM ' + req.param("table_name") + '?limit=' + req.query("limit") , function(err, rows) {
-                    res.json({data: rows});
-                });
+            else{
+                res.json({mesaage: 'either table name is not defined or column name is not defined'})
             }
         });
     }
 };
-exports.add_user = function(req, res) {
-    if (req.param("user") !== '') {
-        connection.query("INSERT INTO users (user_name) VALUES ('" + req.param("user") + "')");
+exports.insert_into = function(req, res) {
+     if (typeof (req.param('db_name')) !== 'undefined' ){
+        connection.query('USE ' + req.param("db_name"), function(err, rows){
+            
+            if ((typeof (req.param('table_name')) !== 'undefined' )){
+ 
+        var columns = eval(req.param('column'));
+        console.log(columns);
+       // if(req.param('primary_key') in req.param('column') ){
+            
+            var query = 'CREATE TABLE ' + req.param('table_name') +' ('+req.param('primary_key')+' int, ' ;
+            for(x in column){
+                query += column[x] + ' varchar(100), ';
+            }
+            query += ' PRIMARY KEY ('+ req.param('primary_key') + ' ) )';
+            
+            
+       // }
+        
     }
-    res.redirect('/users');
+            
+            
+            
+            
+        });
+        
+    }
 
 };
-exports.delete_user = function(req, res) {
-    // connection.query("DELETE FROM users WHERE user_name='"+req.param("user")+"'");
-    console.log(req.query.delete_name);
-    res.redirect('/users');
-};
 
-exports.update = function(req, res) {
-    if (typeof (req.query) !== 'undefined' && typeof (req.query.update_name) !== 'undefined' && req.query.update_name !== '') {
-        console.log(req.query.user_name);
-        connection.query("UPDATE users SET user_name='" + req.query.user_name + "' WHERE id='" + req.query.update_name + "'");
-        res.redirect('/users');
+
+exports.update_byid = function(req, res) {
+    if (typeof (req.param('db_name')) !== 'undefined' ){
+        connection.query('USE ' + req.param("db_name"), function(err, rows){
+            
+            if ((typeof (req.param('table_name')) !== 'undefined' ) && (typeof (req.param('id')) !== 'undefined' )){
+ 
+        var columns = eval(req.param('column'));
+        console.log(columns);
+       // if(req.param('primary_key') in req.param('column') ){
+            
+            var query = 'CREATE TABLE ' + req.param('table_name') +' ('+req.param('primary_key')+' int, ' ;
+            for(x in column){
+                query += column[x] + ' varchar(100), ';
+            }
+            query += ' PRIMARY KEY ('+ req.param('primary_key') + ' ) )';
+            
+            
+       // }
+        
     }
+            
+            
+            
+            
+        });
+        
+    }
+
 };
-exports.delete = function(req, res) {
-    if (typeof (req.query) !== 'undefined' && typeof (req.query.delete_name) !== 'undefined' && req.query.delete_name !== '') {
-        connection.query("DELETE FROM users WHERE id='" + req.query.delete_name + "'");
-        res.redirect('/users');
+exports.delete_byid = function(req, res) {
+    if (typeof (req.param('db_name')) !== 'undefined' ){
+        connection.query('USE ' + req.param("db_name"), function(err, rows){
+        
+        if ((typeof (req.param('table_name')) !== 'undefined' ) && (typeof (req.param('id')) !== 'undefined' ) ){
+            
+            connection.query('DELETE FROM ' + req.param('table_name') + ' WHERE id = '+ req.param('id'), function(err, rows) {
+                    res.json({data: rows});
+              });
+        }
+        
+        
+        
+                
+    });
     }
 };
 exports.create_db = function(req, res){
